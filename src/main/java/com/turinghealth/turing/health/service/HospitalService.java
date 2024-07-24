82888edc1b1e50ffd4abdb3dd64b95af6fb40a44
Axel Barlian <axelbarlian16@gmail.com>
2024-07-24 10:34:47 +0700

    Added new endpoint in hospitals

--- src/main/java/com/turinghealth/turing/health/service/HospitalService.java
+++ src/main/java/com/turinghealth/turing/health/service/HospitalService.java
@@ -1,5 +1,7 @@
 package com.turinghealth.turing.health.service;
 
+import com.turinghealth.turing.health.utils.dto.hospitalDTO.HospitalDTO;
+
 public interface HospitalService {
-    
+    HospitalDTO getHospitals();
 }
