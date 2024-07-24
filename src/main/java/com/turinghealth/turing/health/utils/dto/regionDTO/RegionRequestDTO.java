36165b843a92dacd82fad279dfb13c284410c11f
Axel Barlian <axelbarlian16@gmail.com>
2024-07-24 11:25:38 +0700

    feat: Region controller and added constructor on Region DTOs

--- src/main/java/com/turinghealth/turing/health/utils/dto/regionDTO/RegionRequestDTO.java
+++ src/main/java/com/turinghealth/turing/health/utils/dto/regionDTO/RegionRequestDTO.java
@@ -1,12 +1,12 @@
 package com.turinghealth.turing.health.utils.dto.regionDTO;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegionRequestDTO {
     private String name;
 }
